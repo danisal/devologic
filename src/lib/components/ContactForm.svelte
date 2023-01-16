b8d4deb6c8ff85daaf0330cdcb62b69cc126acdd
danisal <daniel.salvado@gmail.com>
2023-01-16 20:17:26 +0000

    Client side form validation

--- src/lib/components/ContactForm.svelte
+++ src/lib/components/ContactForm.svelte
@@ -36,59 +36,86 @@
 	<label>
 		{name}
 		<input
-			class="placeholder:text-md block w-full rounded bg-slate-700 p-3 text-sm font-normal text-slate-200 placeholder:italic placeholder:text-slate-500"
+			class={[
+				'block w-full rounded border border-slate-700 bg-slate-700 p-3 text-sm font-normal text-slate-200',
+				'focus:border-slate-300 focus:outline-none',
+				'placeholder:text-md placeholder:italic placeholder:text-slate-500',
+				'valid:[&:not(:placeholder-shown)]:border-green-300 [&:not(:placeholder-shown):not(:focus):invalid~span]:inline-block invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500',
+			].join(' ')}
 			name="name"
+			minlength="2"
+			maxlength="64"
 			placeholder={namePlaceholder}
 			required
 			type="text"
 			value={data?.name || ''}
 		/>
-		{#if errors?.name}
-			<span class="rounded bg-red-50 p-1 text-xs font-normal text-red-700">{errors.name[0]}</span>
-		{/if}
+		<span class="hidden rounded bg-red-50 p-1 text-xs font-normal text-red-700" class:inline-block={errors?.name}>
+			{errors?.name[0] ?? 'Name should have between 2 and 64 characters'}
+		</span>
 	</label>
 	<label>
 		Email
 		<input
-			class="placeholder:text-md block w-full rounded bg-slate-700 p-3 text-sm font-normal text-slate-200 placeholder:italic placeholder:text-slate-500"
+			class={[
+				'block w-full rounded border border-slate-700 bg-slate-700 p-3 text-sm font-normal text-slate-200',
+				'focus:border-slate-300 focus:outline-none',
+				'placeholder:text-md placeholder:italic placeholder:text-slate-500',
+				'valid:[&:not(:placeholder-shown)]:border-green-300 [&:not(:placeholder-shown):not(:focus):invalid~span]:inline-block invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500',
+			].join(' ')}
 			name="email"
+			pattern="[a-z0-9._+-]+@[a-z0-9.-]+\.[a-z]&#123;2,}$"
 			placeholder="jane@doe.com"
 			required
 			type="email"
 			value={data?.email || ''}
 		/>
-		{#if errors?.email}
-			<span class="rounded bg-red-50 p-1 text-xs font-normal text-red-700">{errors.email[0]}</span>
-		{/if}
+		<span class="hidden rounded bg-red-50 p-1 text-xs font-normal text-red-700" class:inline-block={errors?.email}>
+			{errors?.email[0] ?? 'Please enter a valid email address'}
+		</span>
 	</label>
 	<label>
 		{subject}
 		<input
-			class="placeholder:text-md block w-full rounded bg-slate-700 p-3 text-sm font-normal text-slate-200 placeholder:italic placeholder:text-slate-500"
+			class={[
+				'block w-full rounded border border-slate-700 bg-slate-700 p-3 text-sm font-normal text-slate-200',
+				'focus:border-slate-300 focus:outline-none',
+				'placeholder:text-md placeholder:italic placeholder:text-slate-500',
+				'valid:[&:not(:placeholder-shown)]:border-green-300 [&:not(:placeholder-shown):not(:focus):invalid~span]:inline-block invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500',
+			].join(' ')}
 			name="subject"
+			minlength="2"
+			maxlength="64"
 			placeholder={subjectPlaceholder}
 			required
 			type="text"
 			value={data?.subject || ''}
 		/>
-		{#if errors?.subject}
-			<span class="rounded bg-red-50 p-1 text-xs font-normal text-red-700">{errors.subject[0]}</span>
-		{/if}
+		<span class="hidden rounded bg-red-50 p-1 text-xs font-normal text-red-700" class:inline-block={errors?.subject}>
+			{errors?.subject[0] ?? 'Subject should have between 2 and 64 characters'}
+		</span>
 	</label>
 	<label>
 		{message}
 		<textarea
-			class="placeholder:text-md w-full rounded bg-slate-700 p-3 text-sm font-normal text-slate-200 placeholder:italic placeholder:text-slate-500"
+			class={[
+				'block w-full rounded border border-slate-700 bg-slate-700 p-3 text-sm font-normal text-slate-200',
+				'focus:border-slate-300 focus:outline-none',
+				'placeholder:text-md placeholder:italic placeholder:text-slate-500',
+				'valid:[&:not(:placeholder-shown)]:border-green-300 [&:not(:placeholder-shown):not(:focus):invalid~span]:inline-block invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500',
+			].join(' ')}
 			id="message"
 			name="message"
 			placeholder={messagePlaceholder}
 			required
 			rows="6"
+			maxlength="500"
+			minlength="10"
 			value={data?.message || ''}
 		/>
-		{#if errors?.message}
-			<span class="rounded bg-red-50 p-1 text-xs font-normal text-red-700">{errors.message[0]}</span>
-		{/if}
+		<span class="hidden rounded bg-red-50 p-1 text-xs font-normal text-red-700" class:inline-block={errors?.message}>
+			{errors?.message[0] ?? 'Message should have between 10 and 500 characters'}
+		</span>
 	</label>
 	<button
 		class="group rounded bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-green-300 via-blue-500 to-purple-600 py-2 transition-colors delay-300 ease-in  hover:from-purple-600 hover:to-green-300  hover:shadow focus:from-purple-600 focus:to-green-300 focus-visible:shadow"

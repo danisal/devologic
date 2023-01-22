8b75b94d342480605d8e1b1d8287c9ff85239a28
danisal <daniel.salvado@gmail.com>
2023-01-22 16:11:52 +0000

    Better styles for privacy policy page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,33 +2,33 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}', './src/app.html'],
 	theme: {
 		extend: {
 			fontFamily: {
 				sans: ['Roboto', ...defaultTheme.fontFamily.sans],
-				serif: ['Nunito', ...defaultTheme.fontFamily.serif]
+				serif: ['Nunito', ...defaultTheme.fontFamily.serif],
 			},
 			animation: {
-				blob: 'blob 8s infinite'
+				blob: 'blob 8s infinite',
 			},
 			keyframes: {
 				blob: {
 					'0%': {
-						transform: 'translate(0px, 0px) scale(1)'
+						transform: 'translate(0px, 0px) scale(1)',
 					},
 					'33%': {
-						transform: 'translate(30px, -50px) scale(1.1)'
+						transform: 'translate(30px, -50px) scale(1.1)',
 					},
 					'66%': {
-						transform: 'translate(-20px, 20px) scale(0.9)'
+						transform: 'translate(-20px, 20px) scale(0.9)',
 					},
 					'100%': {
-						transform: 'translate(0px, 0px) scale(1)'
-					}
-				}
-			}
-		}
+						transform: 'translate(0px, 0px) scale(1)',
+					},
+				},
+			},
+		},
 	},
-	plugins: [require('@tailwindcss/typography')]
+	plugins: [require('@tailwindcss/typography')],
 };

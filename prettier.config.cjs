27c0bd396bfae041ba63904305c69d02b0209dd5
danisal <daniel.salvado@gmail.com>
2023-08-10 22:10:15 +0100

    User prettierrc instead

--- prettier.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	useTabs: true,
-	singleQuote: true,
-	trailingComma: 'es5',
-	printWidth: 120,
-	plugins: [require("prettier-plugin-svelte"), require('prettier-plugin-tailwindcss')],
-};

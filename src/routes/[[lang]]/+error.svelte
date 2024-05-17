c81b74db19ea629a344a7a381533e7f258114d7c
danisal <daniel.salvado@gmail.com>
2024-05-17 15:15:53 +0100

    chore: update dependencies and remove comments in error.svelte file
    - Updated the package.json and pnpm-lock.yaml files with new dependencies.
    - Also, unnecessary comments are removed from the error.svelte file.
    - The reordering of script commands in package.json for better readability. Introduced changes in the versions of dependencies will help to keep the project updated and efficient.

--- src/routes/[[lang]]/+error.svelte
+++ src/routes/[[lang]]/+error.svelte
@@ -1,11 +1,3 @@
-<!--
-  This example requires updating your template:
-
-  ```
-  <html class="h-full">
-  <body class="h-full">
-  ```
--->
 <main class="relative isolate min-h-[100dvh]">
 	<img
 		src="https://images.unsplash.com/photo-1545972154-9bb223aac798?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75"

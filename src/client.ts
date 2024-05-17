213eff97c362f0a58b0b713ae16ea545206dddfa
danisal <daniel.salvado@gmail.com>
2024-05-17 14:36:08 +0100

    refactor: formatting
    - Updated the project formatting for code consistency across multiple files including tsconfig.json, routes, favicon configurations, and client setup.
    - This involves adjustments for tabs, spacing, and line breaks.
    - This will enhance overall readability and maintainability of the codebase.

--- src/client.ts
+++ src/client.ts
@@ -2,7 +2,7 @@ import { HoudiniClient } from '$houdini';
 const HYGRAPH_URL = import.meta.env.VITE_HYGRAPH_URL;
 
 export default new HoudiniClient({
-	url: HYGRAPH_URL
+	url: HYGRAPH_URL,
 
 	// uncomment this to configure the network call (for things like authentication)
 	// for more information, please visit here: https://www.houdinigraphql.com/guides/authentication

a782561640c529dac0662e14dc41453a651c6763
danisal <daniel.salvado@gmail.com>
2024-06-21 18:28:51 +0100

    fix: Remove hardcoded values from contact form
    - The hardcoded values for the fields firstName, lastName, email, and phoneNumber in the contact form have been removed.
    - This is necessary to ensure the fields are empty by default and to prevent any potential privacy issues.

--- src/routes/[[lang]]/contact/+page.svelte
+++ src/routes/[[lang]]/contact/+page.svelte
@@ -79,7 +79,6 @@
 									id="firstName"
 									autocomplete="given-name"
 									class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-east-bay-600 sm:text-sm sm:leading-6"
-									value="Daniel"
 								/>
 							</div>
 							<Error error={form?.errors?.firstName?.[0]} />
@@ -94,7 +93,6 @@
 									id="lastName"
 									autocomplete="family-name"
 									class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-east-bay-600 sm:text-sm sm:leading-6"
-									value="Salvado"
 								/>
 							</div>
 							<Error error={form?.errors?.lastName?.[0]} />
@@ -109,7 +107,6 @@
 									id="email"
 									autocomplete="email"
 									class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-east-bay-600 sm:text-sm sm:leading-6"
-									value="daniel.salvado@gmail.com"
 								/>
 							</div>
 							<Error error={form?.errors?.email?.[0]} />
@@ -125,7 +122,6 @@
 									id="phoneNumber"
 									autocomplete="tel"
 									class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-east-bay-600 sm:text-sm sm:leading-6"
-									value="964268468"
 								/>
 							</div>
 							<Error error={form?.errors?.phoneNumber?.[0]} />
